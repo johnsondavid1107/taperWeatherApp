07fb8e459c3d4305e4dcfed939afbeff08c6ebe0
Ness <johnsondavid1107@gmail.com>
2020-10-20 11:39:55 -0400

    added moment.js support to add todays date.  Created and appended data for city entry to page

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,46 @@
 $(document).ready(function () {
 
     var searchBtn = $(".searchBtn");
+    var date = (moment().format("L"));
 
 
-    searchBtn.on("click", function (){
-        var newEntry = ($("input").val());
+    searchBtn.on("click", function () {
+        var newEntry = ($(".inputF").val());
         var newLiItem = $("<li>");
         newLiItem.addClass("list-group-item");
         newLiItem.text(newEntry);
         $("#place").append(newLiItem);
-       
         
-        
-
-       
-       
-
-
     });
 
 
 
 
 
+    searchBtn.on("click", function () {
+        var appID = "6505ade685218bab7b6f73f294ad0301";
+       
+
+        var cityName = $(".inputF").val();
+        console.log(cityName);
+        var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + appID;
+        $("input").val("");
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            
+            $(".city").text(cityName + " " + " " + "(" + date + ")");
+            $(".temp").text("Current Tempeture: " + response.main.temp);
+            $(".humidity").text("Humidity: " + response.main.humidity);
+
+        })
 
 
+
+
+    });
 
 
 
