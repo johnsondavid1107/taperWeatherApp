df7775ebb193ead4f0c11fc36fd3cf14389a50e7
Ness <johnsondavid1107@gmail.com>
2020-10-22 18:28:15 -0400

    final working copy all aspects HORRAAYYYgit add .

--- assets/script.js
+++ assets/script.js
@@ -2,9 +2,10 @@ $(document).ready(function () {
 
     var searchBtn = $(".searchBtn");
     var date = (moment().format("L"));
+    var appID = "6505ade685218bab7b6f73f294ad0301";
 
     searchBtn.on("click", function process() {
-        var appID = "6505ade685218bab7b6f73f294ad0301";
+
         var cityName = $(".inputF").val();
 
         var newEntry = ($(".inputF").val());
@@ -75,7 +76,7 @@ $(document).ready(function () {
                         var fiveImgTag = $("<img>");
                         var fiveImgUrl = "http://openweathermap.org/img/wn/" + fiveDayIcon + "@2x.png";
                         date = (moment().add(i, 'day').format("L"));
-                    
+
 
                         $("#" + i).html(date + "<br>" + "Tempeture: " + fiveDayTemp.toFixed(0) + "°" + "<br>" + "Humidity: " + response.daily[i].humidity + "%" + "<br>");
                         fiveImgTag.attr("src", fiveImgUrl);
@@ -99,7 +100,7 @@ $(document).ready(function () {
 
                     var history = {
                         city: cityName,
-                        tempeture: tempF,
+                        temperture: tempF,
                         humidity: humidity,
                         windS: windS,
                         uvIndex: uvIndex,
@@ -122,45 +123,118 @@ $(document).ready(function () {
 
     var lastCity = JSON.parse(localStorage.getItem("History"));
 
-    // $(".city").text(lastCity);
+    //can this be a function?  or do I need to set every element to text.
 
-    console.log(lastCity.humidity);
-    // console.log(lastCity.city);
-
-    $("body").on("click", function () {
-        if ($(this) == $("li"));
-        console.log("This worked");
-    
-    });
+    $(".city").text(lastCity.city);
+    // $(".city").append(imgTag);
+    $(".temp").text("Current Tempeture: " + lastCity.temperture.toFixed(0) + "°");
+    $(".humidity").text("Humidity: " + lastCity.humidity + "%");
+    $(".windS").text("Wind Speed: " + lastCity.windS + "m/s");
+    $(".uVI").text("UVI Index: " + lastCity.uvIndex)
 
 
 
-    ///Paolo Onclick the list element 
+    $("#place").on("click", "li", function () {
+
+        console.log($(this).text())
+        var cityName = $(this).text()
+
+        var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + appID;
+
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+            var humidity = response.main.humidity;
+            var windS = response.wind.speed;
+            var img = response.weather[0].icon;
+            var lat = response.coord.lat;
+            var lon = response.coord.lon;
+            var imgTag = $("<img>");
+            var imgUrl = "http://openweathermap.org/img/wn/" + img + "@2x.png";
+            imgTag.attr("src", imgUrl);
+            console.log(imgTag);
+            console.log(img);
+            console.log(response);
+
+            $(".city").text(cityName + " " + " " + "(" + date + ")");
+            $(".city").append(imgTag);
+            $(".temp").text("Current Tempeture: " + tempF.toFixed(0) + "°");
+            $(".humidity").text("Humidity: " + humidity + "%");
+            $(".windS").text("Wind Speed: " + windS + "m/s");
+
+            var fiveDayUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly,alerts&appid=" + appID;
+            $.ajax({
+                url: fiveDayUrl,
+                method: "GET"
+            }).then(function (response) {
+                for (var i = 0; i < 5; i++) {
+
+                    var fiveDayTemp = (response.daily[i].temp.max - 273.15) * 1.80 + 32;
+                    var fiveDayIcon = response.daily[i].weather[0].icon;
+                    var fiveImgTag = $("<img>");
+                    var fiveImgUrl = "http://openweathermap.org/img/wn/" + fiveDayIcon + "@2x.png";
+                    date = (moment().add(i, 'day').format("L"));
+
+
+                    $("#" + i).html(date + "<br>" + "Tempeture: " + fiveDayTemp.toFixed(0) + "°" + "<br>" + "Humidity: " + response.daily[i].humidity + "%" + "<br>");
+                    fiveImgTag.attr("src", fiveImgUrl);
+                    $("#" + i).append(fiveImgTag);
+
+                }
+
+
+
+            })
 
 
 
 
 
 
-    // card bg-primary text-white text-center p-3 data
+        })
+
+
+        //Doesnt work.  Causes the last class read to overide current color. 
+
+        // if (lastCity.uvIndex < 3) {
+        //     $(".uVI").addClass("badge badge-success")
+        // } else if (lastCity.uvIndex > 3 && lastCity.uvIndex < 8) {
+        //     $(".uVI").addClass("badge badge-warning")
+        // } else {
+        //     $(".uVI").addClass("badge badge-danger")
+        // }
+
+        // $(".city").text(lastCity);
+
+        console.log(lastCity);
+        // console.log(lastCity.city);
+
+
+
+
+        // $("body").on("click", function () {
+        //     if ($(this) == $("li"));
+        //     console.log("This worked");
+
+        // });
+
+
+
+        ///Paolo Onclick the list element 
 
 
 
 
 
 
+        // card bg-primary text-white text-center p-3 data
 
 
+
+
+    })
 
 })
-
-
-
-
-
-
-
-
-
-
-
